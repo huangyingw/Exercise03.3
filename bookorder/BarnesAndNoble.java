16ebb85a206e79c32846838695c59c241f49baec
huangyingw@linux_programming <huangyingw@gmail.com>
2013-03-11 21:42:22 -0700

    n

--- bookorder/BarnesAndNoble.java
+++ bookorder/BarnesAndNoble.java
@@ -2,16 +2,4 @@ package bookorder;
 
 public class BarnesAndNoble implements IBookSupplier {
 
-	public double computePrice(String isbn) {
-		double price = Math.random() * 45;
-		System.out.println("Barnes&Noble charges $" + price
-				+ " for book with isbn " + isbn);
-		return price;
-	}
-
-	public void order(Book book) {
-		System.out.println("Book with isbn = " + book.getIsbn()
-				+ " is ordered from Barnes&Noble");
-	}
-
 }
--- bookorder/Borders.java
+++ bookorder/Borders.java
@@ -1,15 +1,4 @@
 package bookorder;
 
-public class Borders implements IBookSupplier{
-	public double computePrice(String isbn) {
-		double price = Math.random() * 45;
-		System.out.println("Borders charges $" + price + " for book with isbn "
-				+ isbn);
-		return price;
-	}
-
-	public void order(Book book) {
-		System.out.println("Book with isbn = " + book.getIsbn()
-				+ " is ordered from Borders");
-	}
+public class Borders implements IBookSupplier {
 }
