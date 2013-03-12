923b0382363b05ea7a3b02d674b377fd5bccfb78
huangyingw@linux_programming <huangyingw@gmail.com>
2013-03-11 21:39:02 -0700

    n

--- bookorder/Application.java
+++ bookorder/Application.java
@@ -11,12 +11,6 @@ public class Application {
 
 		IBookService bookService = context.getBean("bookService",
 				IBookService.class);
-		bookService.buy(new Book("123433267",
-				"Harry Potter and the Order of the Phoenix", "J.K. Rowling"));
-		bookService.buy(new Book("888832678",
-				"Harry Potter and the Sorcerer's Stone", "J.K. Rowling"));
-		bookService.buy(new Book("999923156",
-				"Harry Potter and the Goblet of Fire", "J.K. Rowling"));
 
 	}
 
